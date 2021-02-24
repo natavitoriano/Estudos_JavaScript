fc1bc9e60d16bedc56a7b23907e826fb45e352cc
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-02-24 14:30:37 -0300

    Adicionando o exercício 1 resolvido

---
+++ 01.js
@@ -0,0 +1,8 @@
+//EXERCÍCIO 1
+/*01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma,
+* subtração, multiplicação e divisão desses valores.
+*/
+
+const calcular = (num1, num2) => console.log(`Soma = ${num1 + num2} \nSubtração = ${num1 - num2} \nMultiplicação = ${num1 * num2} \nDivisão = ${num1 / num2}`)
+
+calcular(10,5)
