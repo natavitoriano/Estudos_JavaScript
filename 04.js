da69eeb6e3593d7b66dbd4c8ca4802f259a67e1f
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-02-24 14:54:09 -0300

    Exercício 04, Função que retorna divisão e resto da divisão

---
+++ 04.js
@@ -0,0 +1,8 @@
+//EXERCÍCIO 4
+/*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+*e o resto da divisão destes dois valores.
+*/
+
+const divisao = (num1,num2) => console.log(`Divisão = ${num1/num2}\nResto da divisão = ${num1%num2}`)
+
+divisao(10,5)
