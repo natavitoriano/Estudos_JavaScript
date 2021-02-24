2877006c28bfa189c186084c894e969c5f25309d
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-02-24 14:48:29 -0300

    Adicionando Exercício 3, criar função que retorna a base elevada ao expoente

---
+++ 03.js
@@ -0,0 +1,6 @@
+//Exercício 3
+/*03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+const baseExpoente = (base,expoente) => Math.pow(base,expoente)
+
+console.log(baseExpoente(10,5))
